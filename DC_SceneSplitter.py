8efe8fe2ada4e65f456a68d20b6e9e49199b82b3
colebob9 <colebob9@gmail.com>
2017-06-05 21:12:37 -0500

    detects multiple scenes in dir

--- DC_SceneSplitter.py
+++ DC_SceneSplitter.py
@@ -8,7 +8,8 @@ This script takes scene files in a "scenes" directory and splits them up based o
 TODO:
 Add messages for copying files
 Some extra tweaks to calculating SSP if the benchmark scores are too far from each other
-Tweak messages
+Factor in already rendered SSP
+add `.dump.backup` and `.json.backup` as file types to deal with
 """
 
 import json
@@ -17,9 +18,12 @@ import operator
 import os, shutil
 from collections import OrderedDict
 
+print("")
+print("DistChunky SceneSplitter")
+print("")
+
 # Config
 
-sceneName = "ChunkyTest"
 scenesDir = "scenes/"
 
 scenesForWorkersDir = "DC_ScenesForWorkers"
@@ -31,22 +35,19 @@ benchmarkResults = {"i7-4790K": 11197, "Pentium G645": 2598, "i5-4690": 7599, "T
 
 # End Config
 
-#
-# Reading JSON and figuring out SSP for each computer.
-#
-print("[Chunky Scene Info:] ")
-sceneJsonFile = scenesDir + sceneName + ".json"
 
-with open(sceneJsonFile, 'r') as f:
-    data = json.load(f)
-    sspTarget = data["sppTarget"]
-    print("SPP Target: " + str(sspTarget))
-    
+# Detect all scenes in scenesDir
+allScenes = []
+for file in os.listdir(scenesDir):
+    if file.endswith(".json"):
+        allScenes.append(os.path.splitext(file)[0])
+print("Found Chunky scenes:")
+print(allScenes)
 print("")
 
+# Add all benchmark results together
 added = 0
 print("[Benchmark results:] ")
-# Add all benchmark results together
 for key, value in benchmarkResults.items():
     print(key + ": " + str(value))
     added = value + added
@@ -58,71 +59,88 @@ print("")
 benchmarkResultsPercentage = {}
 for key, value in benchmarkResults.items():
     benchmarkResultsPercentage[key] = value / added
-print("Percentages: " + str(benchmarkResultsPercentage))
+print("Calculated Percentages: " + str(benchmarkResultsPercentage))
+print("")
 
-benchmarkResultsSSP = {}
-for key, value in benchmarkResultsPercentage.items():
-    sspCalc = sspTarget * value
-    benchmarkResultsSSP[key] = int(round(sspCalc, 0))
-    
-    
-print("SSP Estimations: " + str(benchmarkResultsSSP))
-sspAdded = 0
-for key, value in benchmarkResultsSSP.items():
-    sspAdded = value + sspAdded
-print("Added up: " + str(sspAdded))
-
-if not sspAdded == sspTarget:
-    maxSSPKey = max(benchmarkResults.items(), key=operator.itemgetter(1))[0]
-    maxSSPValue = benchmarkResultsSSP.get(maxSSPKey)
-    sspCorrection = sspTarget - sspAdded
-    print("Giving " + maxSSPKey + " " + str(sspCorrection) + " more SSP.")
-    benchmarkResultsSSP[maxSSPKey] = maxSSPValue + sspCorrection
-    print(maxSSPKey + " will now render " + str(benchmarkResultsSSP[maxSSPKey]) + " SSP.")
-    
-    sspAdded = 0
-    for key, value in benchmarkResultsSSP.items():
-        sspAdded = value + sspAdded
-    print("Added up: " + str(sspAdded))
-    
-# Check that numbers are the same
-if not sspAdded == sspTarget:
-    print("Something went wrong, the numbers dont add up right.\nReport this as an issue on GitHub please.")
-    print(sspAdded, "=/=", sspTarget)
-    exit()
-    
-#
-# Splitting scene file into separate scenes for each PC
-#
-
+# Deleting ScenesForWorkersDir
 if deleteScenesForWorkersDir:
     if os.path.exists(scenesForWorkersDir):
         print("Deleting Workers Directory...")
         shutil.rmtree(scenesForWorkersDir)
 
-# Creating worker directories and copy files
+for scene in allScenes:
+    # Create ScenesForWorkersDirs
+    if not os.path.exists(scenesForWorkersDir):
+        os.mkdir(scenesForWorkersDir)
+        print("Made " + scenesForWorkersDir + " directory.")
 
+    #
+    # Reading JSON and figuring out SSP for each computer.
+    #
+    print("\n[Chunky Scene Info:] ")
+    sceneJsonFile = scenesDir + scene + ".json"
 
-if not os.path.exists(scenesForWorkersDir):
-    os.mkdir(scenesForWorkersDir)
-    print("Made " + scenesForWorkersDir + " directory.")
+    print("Scene: " + scene)
+    with open(sceneJsonFile, 'r') as f:
+        data = json.load(f)
+        sspTarget = data["sppTarget"]
+        print("SPP Target: " + str(sspTarget))
 
-sceneFilesExts = [".dump", ".foliage", ".grass", ".json", ".octree"]
-for key, value in benchmarkResultsSSP.items():
-    for f in sceneFilesExts:
-        workerSceneDir = scenesForWorkersDir + "/" + str(key)
-        if not os.path.exists(workerSceneDir):
-            os.mkdir(workerSceneDir)
-            print("Made " + workerSceneDir + " directory.")
-        shutil.copy(scenesDir + sceneName + f, workerSceneDir)
-
-    # Edit json files
-    workerScene = workerSceneDir + "/" + sceneName + ".json"
-    with open(workerScene, 'r') as json_file:
-        workerJson = json.load(json_file, object_pairs_hook=OrderedDict)
+    benchmarkResultsSSP = {}
+    for key, value in benchmarkResultsPercentage.items():
+        sspCalc = sspTarget * value
+        benchmarkResultsSSP[key] = int(round(sspCalc, 0))
         
-        workerJson["sppTarget"] = value
-        with open(workerScene, 'w') as json_file:
-            json.dump((workerJson), json_file, sort_keys=False, indent=2)
-        print("Wrote for: " + key)
-    
+        
+    print("SSP Estimations: " + str(benchmarkResultsSSP))
+    sspAdded = 0
+    for key, value in benchmarkResultsSSP.items():
+        sspAdded = value + sspAdded
+    #print("Added up: " + str(sspAdded)) # for testing that all workers have the same amount of SSP all together
+
+    if not sspAdded == sspTarget:
+        maxSSPKey = max(benchmarkResults.items(), key=operator.itemgetter(1))[0]
+        maxSSPValue = benchmarkResultsSSP.get(maxSSPKey)
+        sspCorrection = sspTarget - sspAdded
+        print("Giving " + maxSSPKey + " " + str(sspCorrection) + " more SSP.")
+        benchmarkResultsSSP[maxSSPKey] = maxSSPValue + sspCorrection
+        print(maxSSPKey + " will now render " + str(benchmarkResultsSSP[maxSSPKey]) + " SSP.")
+        
+        sspAdded = 0
+        for key, value in benchmarkResultsSSP.items():
+            sspAdded = value + sspAdded
+        print("Added up: " + str(sspAdded))
+        
+    # Check that numbers are the same
+    if not sspAdded == sspTarget:
+        print("Something went wrong, the numbers dont add up right.\nReport this as an issue on GitHub please.")
+        print(sspAdded, "=/=", sspTarget)
+        exit()
+        
+    #
+    # Splitting scene file into separate scenes for each PC
+    #
+    print("")
+
+    # Copy files
+
+    sceneFilesExts = [".dump", ".foliage", ".grass", ".json", ".octree"]
+    for key, value in benchmarkResults.items():
+        for f in sceneFilesExts:
+            workerSceneDir = scenesForWorkersDir + "/" + str(key)
+            if not os.path.exists(workerSceneDir):
+                os.mkdir(workerSceneDir)
+                print("Made " + workerSceneDir + " directory.")
+            shutil.copy(scenesDir + scene + f, workerSceneDir)
+            print("Copied: " + scenesDir + scene + f + " to " + workerSceneDir)
+
+        # Edit json files
+        workerScene = workerSceneDir + "/" + scene + ".json"
+        with open(workerScene, 'r') as json_file:
+            workerJson = json.load(json_file, object_pairs_hook=OrderedDict)
+            
+            workerJson["sppTarget"] = value
+            with open(workerScene, 'w') as json_file:
+                json.dump((workerJson), json_file, sort_keys=False, indent=2)
+            print("Split scene \"" + scene + "\" for worker \"" + key + "\"")
+        
